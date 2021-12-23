56f6bda00854c43cf234c4a5023ca2cb9ea29698
Sangenis Pablo <sangenispablo@gmail.com>
2021-12-23 19:53:00 -0300

    Notes en index from LS

--- js/app.js
+++ js/app.js
@@ -1,2 +1,17 @@
+// busco en LS si hay notas
+let notes = JSON.parse(localStorage.getItem("notes"));
 
+// Si no hay notas a notes=[le meto dos notas] y lo guardo en LS
+if (!notes || notes.length === 0) {
+  notes = ["Primera Nota", "Segunda Nota"];
+  localStorage.setItem("notes", JSON.stringify(notes));
+}
 
+// capturo la pizarra de corcho para meterle notas
+const pizarra = document.querySelector(".tablero");
+notes.forEach((element) => {
+  const stickNote = document.createElement("div");
+  stickNote.innerHTML = element;
+  stickNote.classList.add("note-example");
+  pizarra.appendChild(stickNote);
+});
